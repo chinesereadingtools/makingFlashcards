bb79eec3058afb50faaebac29725e0a2e15794c2
David Langevin <dlangevi@uwaterloo.ca>
2022-03-12 11:27:01 -0800

    Load books info using calibre cli (thank you calibre for providing this feature)

--- scripts/lua/lib/BookLibrary.lua
+++ scripts/lua/lib/BookLibrary.lua
@@ -1,42 +1,45 @@
 -- This works with a file exported from calibre containing (authors, cover, title) fields
 
 local config = require "Config"
+local JSON = require "JSON"
 
 local BookLibrary = {}
 
-
 --This function finds the filename when given a complete path
 function GetFilename(fullpath)
-  path, file = fullpath:match("^(.+)/([^/]+)$")
-  return file
+    path, file = fullpath:match("^(.+)/([^/]+)$")
+    return file
 end
 
 function GetFilepath(fullpath)
-  path, file = fullpath:match("^(.+)/([^/]+)$")
-  return path
+    path, file = fullpath:match("^(.+)/([^/]+)$")
+    return path
 end
 
 function loadLibrary()
-  local books = {}
-  local file = assert(io.open(config.catalogue, "r"))
-  for line in file:lines() do
-    local author, cover, title = line:match( '^"(.+)","(.+)","(.+)"$')
-    if author ~= nil then
-      local path = GetFilepath(cover)
+    local books = {}
+    local cmd =
+        "calibredb --library-path '" .. config.library ..
+        "' list -f cover,authors,title --for-machine --sort-by authors"
+    local handle = io.popen(cmd)
+    local result = handle:read("*a")
+    handle:close()
+    booksJson = JSON:decode(result)
+    for a, b in pairs(booksJson) do
+      local author = b.authors
+      local path = GetFilepath(b.cover)
+      local title = b.title
       books[path] = {author = author, title = title}
     end
-  end
-  file:close()
-  return books
+    return books
 end
 
 -- Path -> (author title)
 local Books = loadLibrary()
 function BookLibrary.getBookData(filepath)
-  path = GetFilepath(filepath)
-  return Books[path]
+    path = GetFilepath(filepath)
+    return Books[path]
 end
-
 
 
 return BookLibrary
